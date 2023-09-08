a223ca9b84772a39ca2f39fda1991509cb2030ae
cocreacion3 <cocreacion3@hgm.gov.co>
2023-09-08 10:59:22 -0500

    feat: test5

--- hi.py
+++ hi.py
@@ -1,10 +1,21 @@
 import time
 import sys
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    handlers=[
+        logging.StreamHandler(),  # Log to console
+        logging.FileHandler('/app/logs/app.log')  # Log to file within the container
+    ]
+)
 
 def main():
     while True:
         # print('hi from worker2')
-        sys.stderr.write("hi from worker2\n")
+        # sys.stderr.write("hi from worker2\n")
+        logging.info('hi from worker3')
         time.sleep(5)
 
 if __name__ == "__main__":
